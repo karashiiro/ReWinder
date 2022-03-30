9ad2747b5cb1290ab0bd0e6031334e071c5e2ccb
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-03-30 12:41:39 -0700

    Add monitor enumeration

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 #include <string>
 
+BOOL HandleMonitor(HMONITOR, HDC, LPRECT pRect, LPARAM param)
+{
+    const auto &rect = *pRect;
+    int *monitor = reinterpret_cast<int *>(param);
+    std::cout << "Monitor " << std::to_string(*monitor) << ": (" << std::to_string(rect.top) << ", "
+              << std::to_string(rect.left) << ", " << std::to_string(rect.bottom) << ", "
+              << std::to_string(rect.right) << ")" << std::endl;
+    *monitor++;
+    return TRUE;
+}
+
 BOOL CALLBACK HandleWindow(HWND hwnd, LPARAM)
 {
     auto len = GetWindowTextLength(hwnd);
@@ -10,7 +21,7 @@ BOOL CALLBACK HandleWindow(HWND hwnd, LPARAM)
     {
         std::cout << "Error! Code: " << GetLastError() << std::endl;
         VirtualFree(title, len + 1, MEM_DECOMMIT);
-        return TRUE;
+        return FALSE;
     }
 
     RECT rect{};
@@ -18,10 +29,10 @@ BOOL CALLBACK HandleWindow(HWND hwnd, LPARAM)
     {
         std::cout << GetLastError() << std::endl;
         VirtualFree(title, len + 1, MEM_DECOMMIT);
-        return TRUE;
+        return FALSE;
     }
 
-    if (rect.top == 0 && rect.left == 0 && rect.bottom == 0 && rect.right == 0)
+    if (rect.top == rect.bottom && rect.left == rect.right)
     {
         // Window has no size, so we don't care about it
         VirtualFree(title, len + 1, MEM_DECOMMIT);
@@ -38,6 +49,8 @@ BOOL CALLBACK HandleWindow(HWND hwnd, LPARAM)
 
 int main()
 {
+    auto monitor = 0;
+    EnumDisplayMonitors(nullptr, nullptr, &HandleMonitor, reinterpret_cast<LPARAM>(&monitor));
     EnumWindows(&HandleWindow, 0);
     return 0;
 }
