d3ae10c823aabd5c9f05a26c081e65d4ca7b55f2
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-03-30 16:57:02 -0700

    Window work

--- main.cpp
+++ main.cpp
@@ -2,6 +2,29 @@
 #include <iostream>
 #include <string>
 
+LRESULT HandleWindowMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            FillRect(hdc, &ps.rcPaint, reinterpret_cast<HBRUSH>(COLOR_WINDOW + 1));
+            EndPaint(hWnd, &ps);
+        }
+            return 0;
+        case WM_CLOSE:
+            DestroyWindow(hWnd);
+            return 0;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hWnd, uMsg, wParam, lParam);
+    }
+}
+
 BOOL HandleMonitor(HMONITOR, HDC, LPRECT pRect, LPARAM param)
 {
     const auto &rect = *pRect;
@@ -13,11 +36,11 @@ BOOL HandleMonitor(HMONITOR, HDC, LPRECT pRect, LPARAM param)
     return TRUE;
 }
 
-BOOL CALLBACK HandleWindow(HWND hwnd, LPARAM)
+BOOL CALLBACK HandleWindow(HWND hWnd, LPARAM)
 {
-    auto len = GetWindowTextLength(hwnd);
+    auto len = GetWindowTextLength(hWnd);
     auto title = static_cast<PSTR>(VirtualAlloc(nullptr, len + 1, MEM_COMMIT, PAGE_READWRITE));
-    if (!SUCCEEDED(GetWindowText(hwnd, title, len + 1)))
+    if (!SUCCEEDED(GetWindowText(hWnd, title, len + 1)))
     {
         std::cout << "Error! Code: " << GetLastError() << std::endl;
         VirtualFree(title, len + 1, MEM_DECOMMIT);
@@ -25,7 +48,7 @@ BOOL CALLBACK HandleWindow(HWND hwnd, LPARAM)
     }
 
     RECT rect{};
-    if (!SUCCEEDED(GetWindowRect(hwnd, &rect)))
+    if (!SUCCEEDED(GetWindowRect(hWnd, &rect)))
     {
         std::cout << GetLastError() << std::endl;
         VirtualFree(title, len + 1, MEM_DECOMMIT);
@@ -47,10 +70,33 @@ BOOL CALLBACK HandleWindow(HWND hwnd, LPARAM)
     return TRUE;
 }
 
-int main()
+int WinMain(HINSTANCE hInst, HINSTANCE, PSTR, int nCmdShow)
 {
+    const char *CLASS_NAME = "ReWinder";
+    WNDCLASS wc{};
+    wc.lpfnWndProc = &HandleWindowMessage;
+    wc.hInstance = hInst;
+    wc.lpszClassName = CLASS_NAME;
+    RegisterClass(&wc);
+
+    auto hWnd = CreateWindowEx(0, CLASS_NAME, "ReWinder", WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                               nullptr, nullptr, hInst, nullptr);
+    if (hWnd == nullptr)
+    {
+        return static_cast<int>(GetLastError());
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+
+    MSG msg{};
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     auto monitor = 0;
     EnumDisplayMonitors(nullptr, nullptr, &HandleMonitor, reinterpret_cast<LPARAM>(&monitor));
     EnumWindows(&HandleWindow, 0);
-    return 0;
 }
